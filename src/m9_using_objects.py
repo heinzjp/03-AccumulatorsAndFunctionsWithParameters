46c0b80f64a96c29da9c8c617f7ffc2155b535d9
heinzjp <heinzjp@rose-hulman.edu>
2018-11-29 23:24:11 -0500

    Done

--- src/m9_using_objects.py
+++ src/m9_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Justin Heinz.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,7 +14,9 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
-
+    #two_circles()
+    #circle_and_rectangle()
+    lines()
 
 def two_circles():
     """
@@ -27,13 +29,21 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its green doc-string above.
+    # DONE: 2. Implement this function, per its green doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.pdf  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # -------------------------------------------------------------------------
 
+    window = rg.RoseWindow(400, 400)
+    circle1 = rg.Circle(rg.Point(250,250), 98)
+    circle2 = rg.Circle(rg.Point(100, 100), 40)
+    circle1.fill_color = 'red'
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
 
 def circle_and_rectangle():
     """
@@ -67,7 +77,7 @@ def circle_and_rectangle():
            150.0
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its green doc-string above.
+    # DONE: 3. Implement this function, per its green doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -75,6 +85,31 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # -------------------------------------------------------------------------
+
+    window = rg.RoseWindow(400, 400)
+    circle = rg.Circle(rg.Point(100, 100), 50)
+    rectangle = rg.Rectangle(rg.Point(200, 200), rg.Point(300, 300))
+    circle.fill_color = 'blue'
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    circle.outline_thickness = 8
+    rectangle.outline_thickness = 4
+
+    print(circle.outline_thickness)
+    print(circle.fill_color)
+    print(circle.center)
+    print(circle.center.x)
+    print(circle.center.y)
+
+    center = rectangle.get_center()
+    print(rectangle.outline_thickness)
+    print(rectangle.fill_color)
+    print(rectangle.get_center())
+    print(center.x)
+    print(center.y)
+
+    window.render()
+    window.close_on_mouse_click()
 
 
 def lines():
@@ -99,8 +134,22 @@ def lines():
 
     -- Waits for the user to press the mouse, then closes the window.
     """
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
 
+    window = rg.RoseWindow(400, 400)
+    line1 = rg.Line(rg.Point(10, 10), rg.Point(390, 390))
+    line2 = rg.Line(rg.Point(10, 390), rg.Point(390, 10))
+    line1.thickness = 5
+    line1.attach_to(window)
+    line2.attach_to(window)
+
+    line1_mid = line1.get_midpoint()
+    print(line1_mid)
+    print(line1_mid.x)
+    print(line1_mid.y)
+
+    window.render()
+    window.close_on_mouse_click()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
